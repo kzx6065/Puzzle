7c66f1c76acd8f293c6a729571bedb37cc79e388
kzx6065 <kzx6065@gmail.com>
2022-09-08 09:07:36 +0900

    초기 마우스 테스트 작업

--- Assets/MouseTest.cs
+++ Assets/MouseTest.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class MouseTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnMouseDown()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(gameObject);
     }
 }
